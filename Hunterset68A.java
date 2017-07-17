e3221380e6b408f69a38778bd7e6e59779b4c3f1
Suriyarithik <suriyavedsum@gmail.com>
2017-07-17 21:38:30 +0530

    Create Hunterset68A.java

---
+++ Hunterset68A.java
@@ -0,0 +1,24 @@
+# Hunterset68A
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Hunterset68A
+{
+
+  public static void main(String[] args)
+  {
+int[] a={2,3,4,2,3};
+ArrayList<Integer> num=new ArrayList<Integer>();
+num.add(2);
+num.add(3);
+num.add(4);
+num.add(2);
+num.add(3);
+int large=Collections.max(num);
+int index=num.indexOf(large);
+Collections.sort(num,Collections.reverseOrder());
+int diff=num.get(0)-num.get(num.size()-1);
+System.out.println("Largest element is "+large+" in index "+index+" with difference "+diff);
+}
+}
